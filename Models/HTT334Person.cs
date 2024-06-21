a0205b6f2d8d2e33a8878e4532ebdd4fabd9389e
HTTBAITHI2324 <hoangtin2003@gmail.com>
2024-06-21 15:21:23 +0700

    commit note

---
+++ Models/HTT334Person.cs
@@ -0,0 +1,10 @@
+namespace HTTBaiThiLai.Models
+{
+    public class HTT334Person
+    {
+        public string HTT334PersonId {get; set;}
+        public string HTT334FullName {get; set;}
+        public string HTT334Address {get; set;}
+
+    }
+}
